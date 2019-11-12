d21f1a40cbb0dd3470620bc734c3c9d709233921
cesar18pena <cesar18pena@gmail.com>
2019-11-12 11:12:47 -0400

    Feat: create 'header' component and started the sign-in page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign-up' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      email: '',
+      password: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form>
+          <input />
+        </form>
+
+      </div>
+    )
+  }
+}
+
+export default SignIn
