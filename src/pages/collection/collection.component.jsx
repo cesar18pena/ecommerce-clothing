2a1e2e64d9b5bdc7c622022464fd0845f2959a5f
cesar18pena <cesar18pena@gmail.com>
2019-11-20 10:46:45 -0400

    Feat: change both 'shop' and 'directory' components to use Redux and create the collections overview component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = () => (
+  <div className="collection">
+    <h2>Category Page</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import userReducer from './user/user.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -12,8 +14,10 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
+  cart: cartReducer,
+  directory: directoryReducer,
   user: userReducer,
-  cart: cartReducer
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
