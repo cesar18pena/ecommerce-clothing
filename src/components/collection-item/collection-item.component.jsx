eda029e53aedf43d7c8ea7996970db378b539d39
cesar18pena <cesar18pena@gmail.com>
2019-11-20 15:57:59 -0400

    Feat: display collection of specific category with the parameters send in the URL

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import CustomButton from '../custom-button/custom-button.component';
-import { addItem } from '../../redux/cart/cart.actions';
+import React from "react";
+import { connect } from "react-redux";
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
-import './collection-item.styles.scss';
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ item, addItem }) => {
-  
   const { name, price, imageUrl } = item;
 
   return (
-    <div className='collection-item'>
-      <div 
-        className='image'
+    <div className="collection-item">
+      <div
+        className="image"
         style={{
           backgroundImage: `url(${imageUrl})`
         }}
       />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
         Add to Cart
       </CustomButton>
     </div>
-)}
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
